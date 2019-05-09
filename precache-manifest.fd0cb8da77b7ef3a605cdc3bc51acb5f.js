938372d13e652bdd51e8fe70715e46599565d611
Jinhao Li <jinhao@Jinhaos-MacBook-Pro.local>
2019-05-09 18:09:02 +1000

    Updates

---
+++ precache-manifest.fd0cb8da77b7ef3a605cdc3bc51acb5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112051cae51787d9f59c5cc8b55a4260",
+    "url": "/React&Node/index.html"
+  },
+  {
+    "revision": "f695ddb361c3be4c231a",
+    "url": "/React&Node/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "16386626cbe7527ff923",
+    "url": "/React&Node/static/css/main.e38ca81e.chunk.css"
+  },
+  {
+    "revision": "f695ddb361c3be4c231a",
+    "url": "/React&Node/static/js/2.1807e032.chunk.js"
+  },
+  {
+    "revision": "16386626cbe7527ff923",
+    "url": "/React&Node/static/js/main.952d375d.chunk.js"
+  },
+  {
+    "revision": "3610c0cc428ca1608e3d",
+    "url": "/React&Node/static/js/runtime~main.ef7b08ce.js"
+  }
+]);
